3af49be4f9cb58be05efb87baf21af89f1c2a977
Víctor Oliva <olivarra1@gmail.com>
2019-12-24 15:31:40 +0100

    Update nodejs bindings

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,32 @@
 
-export function getImageBuffer(orientation);
-export function displayImageBuffer(img);
-export function displayGrayImageBuffer(img);
-export function init();
-export function initGray();
-export function clear();
-export function sleep();
-export function colors();
-export const width;
-export const height;
+export interface GDImg {
+  destroy(): void;
+  setPixel(x: number, y: number, color: number): void;
+  line(x1: number, y1: number, x2: number, y2: number, color: number): void;
+
+  // Feelin' lazy
+  [key: string]: (...args: any[]) => void;
+}
+
+export enum speed {
+  normal,
+  quick,
+  quickest
+}
+
+export const colors: {
+  white: number,
+  gray: number,
+  darkgray: number,
+  black: number
+}
+export const width: number;
+export const height: number;
+
+export function getImageBuffer(orientation?: 'portrait' | 'landscape'): Promise<GDImg>;
+export function init(sp?: speed): Promise<void>;
+export function initGray(sp?: speed): Promise<void>;
+export function clear(): Promise<void>;
+export function displayImageBuffer(img: GDImg): Promise<void>;
+export function displayGrayImageBuffer(img: GDImg): Promise<void>;
+export function sleep(): Promise<void>;
--- index.js
+++ index.js
@@ -1,139 +1,95 @@
 const epd4in2 = require('./build/Release/epd2in7');
 const gd = require('node-gd');
 
-// NODE_SET_METHOD(exports, "width", width);
-// NODE_SET_METHOD(exports, "height", height);
-// NODE_SET_METHOD(exports, "init", init);
-// NODE_SET_METHOD(exports, "initGray", initGray);
-// NODE_SET_METHOD(exports, "clear", clear);
-// NODE_SET_METHOD(exports, "display", display);
-// NODE_SET_METHOD(exports, "displayGray", displayGray);
-// NODE_SET_METHOD(exports, "sleep", sleep);
-
-const width = epd4in2.width();
-const height = epd4in2.height();
-
-function getImageBuffer(orientation) {
-	return new Promise(resolve => {
-		let img
-		if (orientation === undefined || orientation === 'portrait') {
-			img = gd.createSync(height, width);
-		} else {
-			img = gd.createSync(width, height);
-		}
-
-		for (let i = 0; i < 256; i++) {
-			img.colorAllocate(i, i, i);
-		}
-
-		return resolve(img);
-	})
-}
-exports.getImageBuffer = getImageBuffer;
-
-function displayImageBuffer(img) {
-	return new Promise(resolve => {
-		let buf = new Buffer.alloc(width * height, 0);
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				let color = img.height == height
-					? img.getPixel(x, y)
-					: img.getPixel(img.width - y, x);
-				if (color < 128) { // black
-					buf[x + y * width] = 0x00;
-				} else { // white
-					buf[x + y * width] = 0xff;
-				}
-			}
-		}
-		epd4in2.display(
-			buf,
-			() => {
-				resolve();
-			}
-		);
-	})
-}
-exports.displayImageBuffer = displayImageBuffer;
-
-function displayGrayImageBuffer(img) {
-	return new Promise(resolve => {
-		let buf = new Buffer.alloc(width * height, 0);
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				let color = img.height == height
-					? img.getPixel(x, y)
-					: img.getPixel(img.width - y, x);
-				buf[x + y * width] = color;
-			}
-		}
-		epd4in2.displayGray(
-			buf,
-			() => {
-				resolve();
-			}
-		);
-	})
-}
-exports.displayGrayImageBuffer = displayGrayImageBuffer;
-
-// function displayPartialImageBuffer(img, x, y, w, h) {
-// 	return new Promise(resolve => {
-// 		let buf = new Buffer.alloc(width * height, 0);
-// 		for (let y = 0; y < height; y++) {
-// 			for (let x = 0; x < width; x++) {
-// 				let color = img.height == height
-// 					? img.getPixel(x, y)
-// 					: img.getPixel(img.width - y, x);
-// 				if (color < 128) { // black
-// 					buf[x + y * width] = 0x00;
-// 				} else { // white
-// 					buf[x + y * width] = 0xff;
-// 				}
-// 			}
-// 		}
-// 		epd4in2.displayPartialFrame(
-// 			buf,
-// 			x, y, w, h,
-// 			() => {
-// 				resolve();
-// 			}
-// 		);
-// 	})
-// }
-// exports.displayPartialImageBuffer = displayPartialImageBuffer; 
-
-exports.init = () => new Promise(resolve => {
-	epd4in2.init(() => {
-		resolve();
-	});
-})
-
-exports.initGray = () => new Promise(resolve => {
-	epd4in2.initGray(() => {
-		resolve();
-	});
-})
-
-exports.clear = () => new Promise(resolve => {
-	epd4in2.clear(() => {
-		resolve();
-	});
-})
-
-exports.sleep = () => new Promise(resolve => {
-	epd4in2.sleep(() => {
-		resolve();
-	});
-})
-
-exports.colors = {
+const width = exports.width = epd4in2.width();
+const height = exports.height = epd4in2.height();
+const colors = exports.colors = {
 	white: 0xFF,
 	gray: 0xC0,
 	darkgray: 0x80,
 	black: 0x00
 }
+const speed = exports.speed = {
+	normal: 0,
+	quick: 1,
+	quickest: 2
+};
 
-exports.width = width;
-exports.height = height;
-exports.gd = gd;
+exports.getImageBuffer = async (orientation = 'portrait') => {
+	const img = await new Promise(resolve =>
+		(orientation === 'portrait')
+			? gd.create(width, height, (error, img) => resolve(img))
+			: gd.create(height, width, (error, img) => resolve(img))
+	);
+
+	for (let i = 0; i < 256; i++) {
+		img.colorAllocate(i, i, i);
+	}
+
+	return img;
+};
+
+const createBuffer = (img, colorMap) => {
+	const buf = new Buffer.alloc(width * height, 0);
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const color = img.height == height
+				? img.getPixel(x, y)
+				: img.getPixel(img.width - y, x);
+			buf[x + y * width] = colorMap(color);
+		}
+	}
+	return buf;
+}
+
+exports.init = (sp = speed.normal) => new Promise(resolve =>
+	epd4in2.init(sp, resolve)
+);
+
+exports.initGray = (sp = speed.normal) => new Promise(resolve =>
+	epd4in2.initGray(sp, resolve)
+);
+
+exports.displayImageBuffer = (img, bounds) => new Promise(resolve => {
+	const buf = createBuffer(img, color => color < 128 ? colors.black : colors.white);
+	if (!bounds) {
+		return epd4in2.display(
+			buf,
+			resolve
+		);
+	}
+	return epd4in2.display(
+		buf,
+		bounds.x,
+		bounds.y,
+		bounds.w,
+		bounds.h,
+		resolve
+	);
+});
+
+exports.displayGrayImageBuffer = (img, bounds) => new Promise(resolve => {
+	const buf = createBuffer(img, color => color);
+	if (!bounds) {
+		return epd4in2.displayGray(
+			buf,
+			resolve
+		);
+	}
+	return epd4in2.displayGray(
+		buf,
+		bounds.x,
+		bounds.y,
+		bounds.w,
+		bounds.h,
+		resolve
+	);
+});
+
+exports.clear = () => new Promise(resolve =>
+	epd4in2.clear(resolve)
+);
+
+exports.sleep = () => new Promise(resolve =>
+	epd4in2.sleep(resolve)
+);
